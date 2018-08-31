69877953d1d97f824d61814e12cc8ba4b6b966eb
majxik <majxik@gmail.com>
2018-08-31 19:09:01 +0200

    filtering out on way (not completed)
    Filering out working out, but for cycles need to be adjusted.

--- logcat_parser.py
+++ logcat_parser.py
@@ -1,23 +1,22 @@
 import sys
 
 arg_len=len(sys.argv)
-#logcat_file=sys.argv[1]
-#argument=sys.argv[2]
-#keywords=sys.argv[3]
 
 help = "Help:.\n" + \
            "-h prints out help containing info about all available switches.\n" + \
-           "-s prints out the time difference between lines containing TEST STARTED and TEST FINISHED.\n" + \
+           "-s prints out the time difference between lines containing 'TEST STARTED' and 'TEST FINISHED'.\n" + \
            "-i <args,...> prints out lines containing all arguments.\n" + \
            "-e <args,...> print out all lines which don't contain any of provided arguments.\n" + \
            "Usage (Python): python logcat_parser.py logcat_file.txt -i word1,word2,word3"
-
+#Decision tree comanding script
+#Print help, if arguments from comand line contain -h switch
 if "-h" in sys.argv:
     print(help)
 elif "-s" in sys.argv and arg_len == 3:
     print("argument -s")
     print([ line for line in open("logcat_file.txt") if "TEST STARTED" in line])
     print([ line for line in open("logcat_file.txt") if "TEST FINISHED" in line])
+#Filter in, number of arguments must be at least 4 (python_script.py textfile.txt switcher word1,word2,word3)
 elif "-i" in sys.argv and arg_len == 4:
     keywords=sys.argv[3]
     keywords_list=keywords.split(",")
@@ -29,9 +28,18 @@ elif "-i" in sys.argv and arg_len == 4:
 
 elif "-e" in sys.argv and arg_len ==4:
     keywords=sys.argv[3]
+    keywords_list=keywords.split(",")
+    for keyword in keywords_list:
+        search_result=[line for line in open("logcat_file.txt") if keyword not in line]
+        print(keyword)
+        for search_result_line in search_result:
+            print(search_result_line)
+
     print("argument -e")
 else:
-    print("argument is invalid, check help for switches")
+    print("argument is invalid, check help for switches by running script with -h switch(logcat_parser.py -h)")
+
+#python logcat_parser.py -i logcat_file.txt I/InstallerConnection,I/Vold
 
 #print("logcat_file: ",logcat_file)
 #print("argument: ",argument)
