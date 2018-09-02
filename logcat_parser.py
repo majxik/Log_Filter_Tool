3a2fdb7eae2c1599b7fa248b73f73565fa5c9a0b
majxik <majxik@gmail.com>
2018-09-02 21:28:43 +0200

    RC - minor changes across script
    All functions are working.

--- logcat_parser.py
+++ logcat_parser.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 from datetime import datetime
 
 arg_len=len(sys.argv)
+log_file=sys.argv[1]
 
 help = "Help:.\n" + \
            "-h prints out help containing info about all available switches.\n" + \
@@ -11,13 +12,12 @@ help = "Help:.\n" + \
            "-i <args,...> prints out lines containing all arguments.\n" + \
            "-e <args,...> print out all lines which don't contain any of provided arguments.\n" + \
            "Usage (Python): python logcat_parser.py logcat_file.txt -i word1,word2,word3"
-#Decision tree comanding script
-#Print help, if arguments from comand line contain -h switch
-if "-h" in sys.argv:
+#Decision tree commanding script
+if "-h" in sys.argv: #Print help, if arguments from command line contain -h switch
     print(help)
-elif "-s" in sys.argv and arg_len == 3:
-    test_started = [ line for line in open("logcat_file.txt") if "TEST STARTED" in line]
-    test_finished = [ line for line in open("logcat_file.txt") if "TEST FINISHED" in line]
+elif "-s" in sys.argv and arg_len == 3: #Print time diff between TEST STARTEd and TEST FINISHED lines
+    test_started = [ line for line in open(log_file) if "TEST STARTED" in line]
+    test_finished = [ line for line in open(log_file) if "TEST FINISHED" in line]
 
     start_time_parsed=datetime.strptime(test_started[0].split(" ===")[0], "%m-%d %H:%M:%S.%f")
     end_time_parsed=datetime.strptime(test_finished[0].split(" ===")[0], "%m-%d %H:%M:%S.%f")
@@ -32,36 +32,22 @@ elif "-s" in sys.argv and arg_len == 3:
     microseconds = timedelta.microseconds
 
     print("Test took: ",years,"years",days,"days",hours,"hours",minutes,"minutes",seconds,"seconds")
-#Filter in, number of arguments must be at least 4 (python_script.py textfile.txt switcher word1,word2,word3)
-elif "-i" in sys.argv and arg_len == 4:
+elif "-i" == sys.argv[2] and arg_len == 4: #Print only rows with arguments given in switch
     keywords=sys.argv[3]
     keywords_list=keywords.split(",")
-    for keyword in keywords_list:
-        search_result=[line for line in open("logcat_file.txt") if keyword in line]
-        print(keyword)
-        for search_result_line in search_result:
-            print(search_result_line)
-
-elif "-e" in sys.argv and arg_len ==4:
+    for line in open(log_file, 'r'):
+        for expected in keywords_list:
+            if expected in line:
+                print(line)
+elif "-e" == sys.argv[2] and arg_len ==4: #Print all rows except rows contains arguments given in switch
     keywords=sys.argv[3]
     keywords_list=keywords.split(",")
-    for keyword in keywords_list:
-        search_result=[line for line in open("logcat_file.txt") if keyword not in line]
-        print(keyword)
-        for search_result_line in search_result:
-            print(search_result_line)
-
-    print("argument -e")
-else:
-    print("argument is invalid, check help for switches by running script with -h switch(logcat_parser.py -h)")
-
-#python logcat_parser.py -i logcat_file.txt I/InstallerConnection,I/Vold
-
-#print("logcat_file: ",logcat_file)
-#print("argument: ",argument)
-#print("keywords: ",keywords)
-#input("press any key to continue")
-
-#s = "11-30 04:39:49.878 === TEST FINISHED ==="
-#match = re.search('\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d{3}', s)
-#date = datetime.datetime.strptime(match.group(), '%m-%d %H:%M:%S:%f')[:-3].date()
+    for line in open(log_file, 'r'):
+        exists = False
+        for keyword in keywords_list:
+            if keyword in line:
+                exists = True
+        if not exists:
+            print(line)
+else: #if non of option is valid, print error message.
+    print("The argument is invalid, check the help for switches by the running script with -h switch(logcat_parser.py -h)")
